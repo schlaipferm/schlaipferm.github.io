aca45a0eee1276306f674b23d1e7ca8496c5939a
Martina Schlaipfer <martina.schlaipfer@gmail.com>
2024-07-24 17:02:23 -0400

    updates to make rendering easier

--- render.R
+++ render.R
@@ -10,9 +10,4 @@ project_dir <- "schlaipferm.github.io"
 ##########################
 ##########################
 
-
 babelquarto::render_website(file.path(parent_dir, project_dir))
-
-unlink(file.path(parent_dir,project_dir,"docs"),recursive=T)
-site <- list.dirs(file.path(parent_dir,project_dir),recursive = F)[2]
-file.rename(site,file.path(parent_dir,project_dir,"docs"))
