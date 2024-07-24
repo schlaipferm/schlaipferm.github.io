0b4f4a837ab56d17498bc9dd8f05bbcad424901b
Martina Schlaipfer <martina.schlaipfer@gmail.com>
2024-07-24 16:30:53 -0400

    Update render.R

--- render.R
+++ render.R
@@ -1,7 +1,18 @@
+####### User Input #######
+##########################
+
+## specify location of GitHub directory
 parent_dir <- "C:/Users/SCHLAIPFER_M/Documents/GitHub"
+
+## specify name of website
 project_dir <- "schlaipferm.github.io"
+
+##########################
+##########################
+
+
 babelquarto::render_website(file.path(parent_dir, project_dir))
 
-unlink(paste(parent_dir,project_dir,"docs",sep="/"),recursive=T)
-site <- list.dirs(paste(parent_dir,project_dir,sep="/"),recursive = F)[2]
-file.rename(site,paste(parent_dir,project_dir,"docs",sep="/"))
+unlink(file.path(parent_dir,project_dir,"docs"),recursive=T)
+site <- list.dirs(file.path(parent_dir,project_dir),recursive = F)[2]
+file.rename(site,file.path(parent_dir,project_dir,"docs"))
